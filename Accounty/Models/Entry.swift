cef7801f53d12123b8835ad0524f31df457fb09b
Fendross <dario.arzuffi@icloud.com>
2026-01-11 11:19:48 +0100

    Refactored to use Swift native ID instead of making up a UUID.

--- Accounty/Models/Entry.swift
+++ Accounty/Models/Entry.swift
@@ -3,31 +3,32 @@ import SwiftData
 
 @Model
 final class Entry {
-    var uuid: UUID
     var timestamp: Date
     var type: String
     var category: String
     var desc: String
-    var currency: String
+    var currency: String = "EUR"
     var amount: Double
     var notes: String
     
-    init(timestamp: Date, type: String, category: String, desc: String, amount: Double, notes: String) {
-        self.uuid = UUID()
+    init(timestamp: Date = .now, type: String = "Expense", category: String = "General", desc: String = "", amount: Double = 0.0, notes: String = "") {
         self.timestamp = timestamp
         self.type = type
         self.category = category
         self.desc = desc
-        self.currency = "EUR"
         self.amount = amount
         self.notes = notes
     }
-    
+
+    var month: String {
+        timestamp.formatted(.dateTime.month().year())
+    }
+
     func toStringFull() -> String {
-        return "\(timestamp.ISO8601Format()) - \(type) - \(category) - \(desc) - \(currency) \(amount) - \(notes)"
+        "\(timestamp.ISO8601Format()) - \(type) - \(category) - \(desc) - \(currency) \(String(format: "%.2f", amount)) - \(notes)"
     }
     
     func toStringLabel() -> String {
-        return "\(timestamp.ISO8601Format()) - \(type) - \(category) - \(amount)"
+        "\(timestamp.formatted(date: .abbreviated, time: .omitted)) - \(category) - \(currency)\(String(format: "%.2f", amount))"
     }
 }
