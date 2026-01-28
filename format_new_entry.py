c246c774d85a59fd56180021f800df0693c03ba5
Fendross <dario.arzuffi@icloud.com>
2026-01-28 21:11:31 +0100

    User now can use the helper script to format more strings in one go.

--- format_new_entry.py
+++ format_new_entry.py
@@ -5,9 +5,7 @@ def get_input(prompt, default=""):
     value = input(prompt).strip()
     return value if value else default
 
-def main():
-    print("--- Accounty Entry Helper ---")
-    
+def build_csv_string():
     # 1. Date (Defaults to today in yyyy-mm-dd format)
     today = datetime.now().strftime("%Y-%m-%d")
     date = get_input(f"Date (yyyy-mm-dd) [{today} will be considered if input is invalid]: ", today)
@@ -36,10 +34,41 @@ def main():
 
     # Format into CSV string: timestamp,type,category,desc,amount,notes
     csv_string = f"{date},{entry_type},{category},{desc},{amount},{notes}"
-    
-    print("\n--- Copy the line below ---")
-    print(csv_string)
-    print("---------------------------\n")
+
+    return csv_string
+
+
+def main():
+    print("--- Accounty Entry Helper ---")
+
+    csv_strings = []
+    while True:
+        # Assuming at least one string needs to be added, I'll build it straight away.
+        new_csv_string = build_csv_string()
+        if not new_csv_string:
+            continue
+
+        print("\n--- String that has just been created and formatted ---")
+        print(new_csv_string)
+        print("---------------------------\n")
+
+        # Update the DS.
+        csv_strings.append(new_csv_string)
+
+        # Go again if user asks to.
+        another_one = get_input("Do you want to format another one? (y/n) ")
+        if another_one.lower() == 'n':
+            if len(csv_strings) > 1:
+                print("\n--- Copy and paste these strings to the csv file ---")
+            else:
+                print("\n--- Copy and paste the string to the csv file ---")
+
+            for csv_str in csv_strings:
+                print(csv_str)
+            
+            print("---------------------------\n")
+            break
+        
 
 if __name__ == "__main__":
     try:
